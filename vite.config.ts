947a82c653853930ec1079f471d0bf3be6a32b38
hex <hex@cowboy.dev>
2025-07-29 11:04:33 -0600

    add repro config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: { port: 6001, headers: { Test: "test" } },
   plugins: [react(), cloudflare()],
-})
+});
